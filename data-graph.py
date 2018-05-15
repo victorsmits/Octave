73a0c44e90905a0f6d23430a198384ac1e1e55d5
victorsmits <victor.smits@student.vinci.be>
2018-05-15 22:35:48 +0200

    numpy

---
+++ data-graph.py
@@ -0,0 +1,27 @@
+from numpy import loadtxt, polyfit, polyval, linspace
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+pp = PdfPages('plot.pdf')
+data = loadtxt("data.txt")
+x = data[:, 0]
+label = ["CO", "GE", "EM"]
+student = 0
+for i in range(1, 4):
+    y = data[:, i]
+    a = polyfit(x, y, 3)
+    plt.plot(x, polyval(a, x), '--')
+    plt.plot(x, y, 'o', label=label[i - 1])
+    student += y
+
+plt.legend(bbox_to_anchor=(1, 1))
+plt.title('Evolution du nombre d\'etudiants par année')
+pp.savefig()
+plt.close()
+
+b = polyfit(x, student, 2)
+plt.plot(x, student, 'o', label="TOTAL")
+plt.plot(x, polyval(b, x), '--')
+
+pp.savefig()
+pp.close()
